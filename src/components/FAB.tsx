64812fa7e7fd4255d9fc4bd39689f57394b033a8
ValentinAngelov17 <109295073+ValentinAngelov17@users.noreply.github.com>
2025-03-15 20:45:07 +0200

    Added link to FAB button for banner form

--- src/components/FAB.tsx
+++ src/components/FAB.tsx
@@ -1,15 +1,20 @@
 import { Button } from '@mui/joy'
 import Add from '@mui/icons-material/Add'
+import {Link} from "react-router-dom" 
 
 export default function FAB() {
     return (
-        <Button style={{
-            position: 'fixed',
-            right: '72px',
-            bottom: '32px',
-            height: '72px',
-            borderRadius: '50%',
-        }} size="lg">
+        <Button
+            component={Link}
+            to="/banners/create"
+            style={{
+                position: 'fixed',
+                right: '72px',
+                bottom: '32px',
+                height: '72px',
+                borderRadius: '50%',
+            }} 
+            size="lg">
             <Add />
         </Button>
     )
